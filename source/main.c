7e64275a793c2db21d2cad2b852697ccf5c1f0d4
Abdellah El Morabit <nsrddyn@gmail.com>
2025-07-23 11:22:24 +0200

    maocs now returns cpu name

--- source/main.c
+++ source/main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
-#include "lib/linux/cpu.h"
+#include "cpu.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char* argv[]) {
 
-    if (argc > 0){
-
-        printf("%s", argv[1]);
+    if (argc > 0)
+    {
+        switch (*argv[1]){
+            
+            case "name":
+                char *name = cpu_name(void);
+                printf("%s", &name);
+                break;
+            case "frequency": 
+                float frequency = cpu_freq(void);
+                printf("%f", frequency);
+                break;
+            default: 
+                return 1;
+        }
+        return 1;
     }
+
+    return 0;
 }
