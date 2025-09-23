5b3865eb257cf98ee0de8b4fee14446e2deb1d4d
nasr <nsrddyn@gmail.com>
2025-09-24 00:57:55 +0200

    ram bug fixes

--- source/linux/modules/ram.c
+++ source/linux/modules/ram.c
@@ -1,53 +1,10 @@
-// OSX
-#ifdef __APPLE__
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/sysctl.h>
-#include <sys/types.h>
-#include <math.h>
-
-#define CONVERT_BYTES_TO_GIGABYTES 107374182
-
-typedef struct {
-    unsigned long mem_size;
-} ram;
-
-unsigned long get_total(void);
-
-unsigned long get_usage(void);
-
-
-unsigned long get_usage(void) {
-    struct rusage usage;
-    if (0 == getrusage(RUSAGE_SELF, &usage))
-        return usage.ru_maxrss / CONVERT_BYTES_TO_GIGABYTES;
-    else
-        return 0;
-}
-
-unsigned long get_mem(void) {
-    int mib[2];
-    size_t size;
-    uint64_t ram_size;
-
-    mib[0] = CTL_HW;
-    mib[1] = HW_MEMSIZE;
-
-    size = sizeof(ram_size);
-    if (sysctl(mib, 2, &ram_size, &size, NULL, 0) == -1) {
-        perror("sysctl");
-    }
-
-    return ram_size;
-}
-
-#endif
-
 #ifdef __gnu_linux__
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/sysinfo.h>
 
 typedef struct {
     unsigned long mem_size;
@@ -57,14 +14,14 @@ unsigned long get_total(void);
 unsigned long get_usage(void);
 
 
-int main() {
+int main(int argc, char** argv) {
     printf("compiled for __gnu_linux__");
     if (argc > 1) {
         if (strcmp(argv[1], "total") == 0) {
             printf("Get the total ram usage");
             while (1) {
                 sleep(1);
-                printf("%s", get_total());
+                printf("%lu", get_total());
             }
         } else if (strcmp(argv[1], "name") == 0) {
             printf("Get the ram usage");
